99236a7e56dc56771cf42720931df23e9bfb700c
mvfflin <mynameisfatihul@gmail.com>
2023-12-21 21:28:14 +0700

    added message us section

--- js/script.js
+++ js/script.js
@@ -11,3 +11,28 @@ function menuClick() {
   const checkbox = document.getElementById("enable-navigation");
   checkbox.checked = false;
 }
+
+function submitMessage() {
+  const name = document.getElementById("name");
+  const birth = document.getElementById("birth");
+  const gender = document.querySelector("input[name='gender']:checked");
+  const pesan = document.getElementById("pesan");
+
+  if (!name.value || !birth.value || !gender.value || !pesan.value) {
+    alert("Tolong isi semua field/kolom yang ada.");
+  } else {
+    const time = document.getElementById("current-date");
+    const nameResult = document.getElementById("name-result");
+    const birthResult = document.getElementById("gender-result");
+    const pesanResult = document.getElementById("pesan-result");
+    const now = new Date();
+
+    time.innerHTML = `Current Date : ${new Intl.DateTimeFormat("id", {
+      dateStyle: "full",
+      timeStyle: "full",
+    }).format(now)}`;
+    nameResult.innerHTML = `Nama : ${name.value}`;
+    birthResult.innerHTML = `Tanggal lahir : ${birth.value}`;
+    pesanResult.innerHTML = `Pesan : ${pesan.value}`;
+  }
+}
